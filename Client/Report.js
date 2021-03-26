f0569536c256030bb7ea74bc38b1a66d20e8b89b
eric97li <eric97li@gmail.com>
2021-03-26 15:12:32 -0500

    Create Report UI

--- Client/Report.js
+++ Client/Report.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, TextInput, View, Text } from 'react-native';
+import { StyleSheet,TouchableOpacity, TextInput, View, Text } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Geocoder from 'react-native-geocoding';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default class Report extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			incident: "",
+			incidentDetails: "",
+			searchString: ""
+		};
 	}
 
 	componentDidMount() {
@@ -15,17 +22,178 @@ export default class Report extends Component {
 
 	componentDidUpdate() {}
 
+	updateLocation(searchString) {
+		console.log('Searching for' + searchString);
+		Geocoder.from(searchString)
+			.then((json) => {
+				var location = json.results[0].geometry.location;
+				let newRegion = {};
+				newRegion.latitude = Number(location.lat.toFixed(4));
+				newRegion.longitude = Number(location.lng.toFixed(4));
+				newRegion.latitudeDelta = 0.2;
+				newRegion.longitudeDelta = 0.2;
+				this.setState({ region: newRegion });
+			})
+			.catch((error) => console.warn(error));
+	}
+
+	submitReport() {
+		alert("Report Submitted!")
+	}
+
 	render() {
 		return (
 			<View
 				style={{
-					width: '50%',
-					height: '95%',
+					width: '100%',
+					height: '90%',
 					justifyContent: 'center',
 					alignSelf: 'center',
 					alignContent: 'center',
 					alignItems: 'center',
-				}}></View>
+				}}>
+			<View style={{marginBottom: '15%'}}></View>
+
+			<View style={{marginBottom: '15%'}}>
+				<Text style={{fontSize:35, justifyContent: 'center',
+								alignSelf: 'center',
+								alignContent: 'center',
+								alignItems: 'center',}}>Report Incident</Text>
+			</View>
+
+		
+			<DropDownPicker
+    			items={[
+					{label: 'Ableism', value: 'Ableism'},
+					{label: 'Ageism', value: 'Ageism'},
+					{label: 'Human Rights', value: "Human Rights"},
+					{label: 'Racism', value: 'Racism'},
+					{label: 'Religious', value: 'Religious'},
+					{label: 'Sexism', value: 'Sexism'},
+					{label: 'Violence', value: 'Violence'},
+					{label: 'Workplace', value: 'Workplace'},
+				]}
+					placeholder={"Select Incident"}
+					defaultValue={this.state.incident}
+					containerStyle={{width: 250, height: 60}}
+					style={{backgroundColor: '#fafafa'}}
+					itemStyle={{
+						justifyContent: 'flex-start'
+				}}
+					dropDownStyle={{backgroundColor: '#fafafa'}}
+					onChangeItem={item => this.setState({
+						incident: item.value
+				})}
+			/>
+			
+			<View style={{marginBottom:"15%"}}></View>
+			
+			<DropDownPicker
+				items={[
+					{label: 'Segregation', value: 'Segregation'},
+					{label: 'Harassment', value: 'Harassment'},
+					{label: 'Infringement/breach of law', value: "Infringement/breach of law"},
+					{label: 'Sexual assault', value: 'Sexual assault'},
+					{label: 'Discrimination', value: 'Discrimination'},
+					{label: 'Maternity', value: 'Maternity'},
+					{label: 'Occupation', value: 'Occupation'},
+					{label: 'Health', value: 'Health'},
+				]}
+					placeholder={"Select Incident Details"}
+					defaultValue={this.state.incidentDetails}
+					containerStyle={{width: 250, height: 60}}
+					style={{backgroundColor: '#fafafa'}}
+					itemStyle={{
+						justifyContent: 'flex-start'
+				}}
+					dropDownStyle={{backgroundColor: '#fafafa'}}
+					onChangeItem={item => this.setState({
+						incidentDetails: item.value
+				})}
+			/>	
+
+				<View style={{marginBottom:"15%"}}></View>
+
+				<View style={styles.searchSection}>
+					<Ionicons
+						style={styles.searchIcon}
+						name='search'
+						size={20}
+						color='#000'
+						onPress={() => this.updateLocation(this.state.searchString)}
+					/>
+					<TextInput
+						style={styles.input}
+						placeholder='Location'
+						onChangeText={(searchString) => {
+							this.setState({ searchString });
+						}}
+						onSubmitEditing={() => this.updateLocation(this.state.searchString)}
+					/>
+				</View>
+
+				<View style={{ marginTop: '15%', width: '80%' }}>
+					<TouchableOpacity
+						style={{
+							borderWidth: 1,
+							height: 42,
+							width: '60%',
+							justifyContent: 'center',
+							alignItems: 'center',
+							borderRadius: 40,
+							backgroundColor: 'black',
+							alignSelf: 'center',
+							textAlign: 'center',
+						}}
+						onPress={() => this.submitReport()}>
+						<Text style={{ color: 'white' }}> Confirm Report </Text>
+					</TouchableOpacity>
+				</View>
+
+
+				</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	button: {
+		justifyContent: 'center',
+		alignItems: 'center', // Centered horizontally
+		justifyContent: 'center', //Centered vertically
+		height: '5%',
+		width: 200,
+		margin: 5,
+		backgroundColor: 'black',
+		padding: 5,
+		borderRadius: 40,
+		borderWidth: 1,
+	},
+	input: {
+		padding: 2,
+		margin: 5,
+		height: 2,
+		width: '20%',
+		borderColor: 'black',
+		borderWidth: 1,
+	},
+	searchSection: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#fff',
+	},
+	searchIcon: {
+		padding: 10,
+	},
+	input: {
+		flex: 1,
+		paddingTop: 5,
+		paddingRight: 5,
+		paddingBottom: 5,
+		paddingLeft: 0,
+		backgroundColor: '#fff',
+		color: '#424242',
+	},
+});
